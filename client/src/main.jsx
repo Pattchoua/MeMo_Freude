db50b83bd4285cd9d7d45038c954d1596e4fdd91
Patrick Tchoua <tchouapatrick@gmail.com>
2023-09-17 18:16:47 +0200

    react-dom

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
-import AuthProvider from './context/Auth.jsx';
+import AuthProvider from './context/Auth.jsx';;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
