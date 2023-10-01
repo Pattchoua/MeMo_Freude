2764d6d320d67300d00a8f4ecdc51e2b41901825
Patrick Tchoua <tchouapatrick@gmail.com>
2023-10-01 15:44:08 +0200

    favicon

--- client/src/Authentication/Login.jsx
+++ client/src/Authentication/Login.jsx
@@ -1,18 +1,18 @@
-import { useState, useContext } from 'react';
-import { AuthContext } from '../context/Auth';
-import { Navigate, Link } from 'react-router-dom';
+import { useState, useContext } from "react";
+import { AuthContext } from "../context/Auth";
+import { Navigate, Link } from "react-router-dom";
 
 function Login(props) {
   const context = useContext(AuthContext);
 
   // State to manage user input
   const [user, setUser] = useState({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   });
 
   // State to manage password length message
-  const [passwordLengthMessage, setPasswordLengthMessage] = useState('');
+  const [passwordLengthMessage, setPasswordLengthMessage] = useState("");
 
   // Handle input changes
   const handleChange = (e) => {
@@ -20,11 +20,13 @@ function Login(props) {
     setUser({ ...user, [name]: value });
 
     // Check password length and set the message
-    if (name === 'password') {
+    if (name === "password") {
       if (value.length < 8) {
-        setPasswordLengthMessage('Passwort muss mindestens 8 Zeichen lang sein');
+        setPasswordLengthMessage(
+          "Passwort muss mindestens 8 Zeichen lang sein"
+        );
       } else {
-        setPasswordLengthMessage('');
+        setPasswordLengthMessage("");
       }
     }
   };
@@ -42,7 +44,7 @@ function Login(props) {
   };
 
   // Define a custom class for the form background color
-  const formBackgroundColor = context.loading ? 'bg-white' : 'bg-black/60';
+  const formBackgroundColor = context.loading ? "bg-white" : "bg-black/60";
 
   // Redirect to postsListing Page if user is already authenticated
   if (!context.loading && context.user) {
@@ -52,19 +54,28 @@ function Login(props) {
   // Render the login form as a modal overlay
   if (!context.loading && !context.user) {
     return (
-      <div className={`fixed top-0 left-0 w-full h-full flex items-center justify-center z-50 ${formBackgroundColor}`}>
+      <div
+        className={`fixed top-0 left-0 w-full h-full flex items-center justify-center z-50 ${formBackgroundColor}`}
+      >
         <div className="w-96 p-8 bg-white rounded-lg shadow-lg relative z-50">
           {context.errors?.message}
           <h2 className="text-2xl font-semibold mb-4">Login</h2>
 
           {/* Display a message to register if the user is not registered */}
           <p className="text-gray-700 mb-4">
-            Noch nicht registriert? <Link to="/register" className="text-primary hover:underline">Jetzt registrieren</Link>.
+            Noch nicht registriert?{" "}
+            <Link to="/register" className="text-primary hover:underline">
+              Jetzt registrieren
+            </Link>
+            .
           </p>
 
           <form onSubmit={handleSubmit}>
             <div className="mb-4">
-              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
                 E-Mail:
               </label>
               <input
@@ -79,7 +90,10 @@ function Login(props) {
             </div>
 
             <div className="mb-6">
-              <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
                 Passwort:
               </label>
               <input
@@ -91,7 +105,9 @@ function Login(props) {
                 required
                 className="mt-1 p-2 w-full border rounded-md"
               />
-              {passwordLengthMessage && <p className="text-red-500">{passwordLengthMessage}</p>}
+              {passwordLengthMessage && (
+                <p className="text-red-500">{passwordLengthMessage}</p>
+              )}
             </div>
 
             <div className="flex justify-between">
